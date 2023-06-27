aadd92900c1f653cfdbee5e4f98ee0844dbec42c
Fernando Schettini <FernandoSchettini@outlook.com>
2023-06-27 14:59:55 -0300

    [:sparkles:FEAT]: material added

---
+++ Data Structures/Trees/SegmentTree/Algoritmos/LineSwepSeg.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define MAXN 3000
+
+using namespace std;
+
+struct Event {
+    int x;
+    int y1, y2;
+    int type; // 0 for start event, 1 for end event
+};
+
